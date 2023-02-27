6c6d9b941e4bd82d38d538959fc4fc83660ca26b
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-27 16:01:22 +0100

    Fix validator

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,26 +4,41 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Book;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
+
 
 class BookController extends Controller
 {
 
-    public function getValidated($request) {
-        
-        return $request->validate([
-            "title" => "required|string|min:2|max:100",
-            "author" => "nullable|string|max:100",
-            "publication_date" => "nullable|date",
-            "description" => "nullable|string",
-            "genre" => "required|string|max:100",
-            "cover_image" => "nullable|url",
-            "ISBN" => "required|unique|string|max:13",
-            "price" => "required|numeric",
-            "editor" => "required|string|max:100",
-        ]);
-    }
-    
+    public $validator = [
+        "title" => "required|string|min:2|max:100",
+        "author" => "nullable|string|max:100",
+        "publication_date" => "nullable|date",
+        "description" => "nullable|string",
+        "genre" => "required|string|max:100",
+        "cover_image" => "nullable|url",
+        "ISBN" => "required|unique|string|max:13",
+        "price" => "required|numeric",
+        "editor" => "required|string|max:100",
+    ];
+
+    public $errorMsg = [
+        'title.required' => 'Il titolo è obbligatorio.',
+        'title.min' => 'Il titolo deve essere di almeno due caratteri.',
+        'title.max' => 'Il titolo non deve essere più di 100 caratteri.',
+        'author.max' => "L'autore non deve avere più di 100 caratteri.",
+        'publication_date' => 'La data di uscita deve essere valida.',
+        'price.required' => 'Il prezzo è obbligatorio.',
+        "cover_image.required" => 'La copertina è richiesta.',
+        'ISBN.required' => "L'ISBN è richiesto",
+        'ISBN.unique' => "L'ISBN deve essere unico",
+        'ISBN.max' => "L'ISBN deve essere di 13 caratteri",
+        'editor.required' => "L'editore è richiesto.",
+        'editor.max' => "L'editore non deve avere più di 100 caratteri.",
+    ];
+
+
 
     /**
      * Display a listing of the resource.
@@ -56,9 +71,7 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        $this->getValidated($request);
-        
-        $data = $request->all();
+        $data = $request->validate($this->validator, $this->errorMsg);
         $newBook = new Book();
         $newBook->fill($data);
         $newBook->save();
@@ -99,10 +112,13 @@ class BookController extends Controller
      */
     public function update(Request $request, Book $book)
     {
-        $this->getValidated($request);
-        
-        $data = $request->all();
-        $book->update($data);
+        // $this->getValidated($request);
+        $rules = $this->validator;
+        $rules['ISBN'] = ['required', 'string', 'max:13', Rule::unique('books')->ignore($book->id)];
+
+        $editData = $request->validate($rules, $this->errorMsg);
+
+        $book->update($editData);
 
         return redirect()->route('admin.books.index', compact('book'))->with('message', 'Elemento modificato con successo')->with('alert-type', 'success');
     }
