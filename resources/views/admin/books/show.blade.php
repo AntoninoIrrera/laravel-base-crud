f64d71cc9d21ac1f9027836015ff23ac93e7bff0
MonicaDeBona <debona.monica@outlook.it>
2023-02-27 15:58:13 +0100

    add icons on show

--- resources/views/admin/books/show.blade.php
+++ resources/views/admin/books/show.blade.php
@@ -23,16 +23,19 @@
                 </ul>
                 <div class="actions d-flex justify-content-between w-100">
                     <div class="main-actions">
-                        <a href="{{ route('admin.books.index', $book->id) }}" class="btn btn-primary">Back</a>
+                        <a href="{{ route('admin.books.index', $book->id) }}" class="btn btn-primary"><i
+                                class="fa-solid fa-arrow-left"></i>Back</a>
                     </div>
                     <div class="secondary-actions">
-                        <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-warning">Edit</a>
+                        <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-warning"><i
+                                class="fa-solid fa-edit"></i>Edit</a>
                         <form class="d-inline-block form-delete double-confirm"
                             action="{{ route('admin.books.destroy', $book->id) }}" method="POST"
                             data-element-name="{{ $book->title }}">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" title="Delete" class="btn btn-danger">Delete</button>
+                            <button type="submit" title="Delete" class="btn btn-danger"><i
+                                    class="fa-solid fa-trash"></i>Delete</button>
                         </form>
                     </div>
                 </div>
