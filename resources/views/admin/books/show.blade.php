1dd3bcb38086cf190fdc6244e2ceb6d2c4ea86a4
MonicaDeBona <debona.monica@outlook.it>
2023-02-27 12:45:50 +0100

    linked buttons

--- resources/views/admin/books/show.blade.php
+++ resources/views/admin/books/show.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.admin')
 
+@include('partials.popup')
+
 @section('content')
     <div class="container">
         <div class="card mb-3 mt-5">
             <div class="card-header text-center">
-                <h4 class="card-title">{{ $book->title }}</h4>
+                <h3 class="card-title">{{ $book->title }}</h3>
             </div>
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <div class="card-image">
+                <div class="card-image py-5">
                     <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
                 </div>
                 <p class="card-text text-center">{{ $book->description }}</p>
-                <hr>
                 <p class="card-text text-center">
                     <small class="text-muted">Author: {{ $book->author }}</small><br>
                     <small class="text-muted">Pubblication date: {{ $book->pubblication_date }}</small><br>
@@ -20,13 +21,15 @@
                     <small class="text-muted">ISBN: {{ $book->ISBN }}</small><br>
                     <small class="text-muted">Price: {{ $book->price }}</small>
                 </p>
-                <div class="actions d-flex justify-content-between w-100">
+                <div class="actions d-flex w-100">
                     <div class="main-actions">
-                        <a href="" class="btn btn-primary">Edit</a>
-                        <form action="" method="POST" class="d-inline-block">
+                        <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-primary">Edit</a>
+                        <form class="d-inline-block form-delete double-confirm"
+                            action="{{ route('admin.books.force-delete', $book->id) }}" method="POST"
+                            data-element-name="{{ $book->title }}">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-md btn-danger">Delete</button>
+                            <button type="submit" title="Delete" class="btn btn-danger">Delete</button>
                         </form>
                     </div>
                 </div>
