6783405e9b2808c98ca5076700cf5fb05ae3df06
MonicaDeBona <debona.monica@outlook.it>
2023-02-27 16:22:07 +0100

    add icons

--- resources/views/admin/books/index.blade.php
+++ resources/views/admin/books/index.blade.php
@@ -3,52 +3,55 @@
 @include('partials.popup')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div class="row">
-        <div class="col-12 mt-3 text-end">
-            <a href="{{ route('admin.books.create') }}" class="btn btn-secondary">Create</a>
+        <div class="row">
+            <div class="col-12 mt-3 text-end">
+                <a href="{{ route('admin.books.create') }}" class="btn btn-secondary"><i class="fa-solid fa-plus"></i></a>
+            </div>
+        </div>
+
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">ISBN</th>
+                    <th scope="col">title</th>
+                    <th scope="col">author</th>
+                    <th scope="col">price</th>
+                    <th scope="col">operation</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book)
+                    <tr>
+                        <th scope="row">{{ $book['ISBN'] }}</th>
+                        <td>{{ $book['title'] }}</td>
+                        <td>{{ $book['author'] }}</td>
+                        <td>{{ $book['price'] }}</td>
+                        <td>
+                            <a href="{{ route('admin.books.show', $book->id) }}" class="btn btn-primary"><i
+                                    class="fa-solid fa-eye"></i></a>
+                            <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-warning"><i
+                                    class="fa-solid fa-edit"></i></a>
+                            <form class="d-inline-block form-delete" action="{{ route('admin.books.destroy', $book->id) }}"
+                                method="POST" data-element-name="{{ $book->title }}">
+                                @csrf
+                                @method('DELETE')
+                                <button title="Delete" class="btn btn-danger"><i class="fa-solid fa-edit"></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="row">
+            <div class="col-12">
+                {{ $books->links() }}
+            </div>
         </div>
     </div>
-
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th scope="col">ISBN</th>
-                <th scope="col">title</th>
-                <th scope="col">author</th>
-                <th scope="col">price</th>
-                <th scope="col">operation</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($books as $book)
-            <tr>
-                <th scope="row">{{ $book['ISBN'] }}</th>
-                <td>{{ $book['title'] }}</td>
-                <td>{{ $book['author'] }}</td>
-                <td>{{ $book['price'] }}</td>
-                <td>
-                    <a href="{{ route('admin.books.show', $book->id) }}" class="btn btn-primary">Show</a>
-                    <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-warning">Edit</a>
-                    <form class="d-inline-block form-delete" action="{{ route('admin.books.destroy', $book->id) }}" method="POST" data-element-name="{{ $book->title }}">
-                        @csrf
-                        @method('DELETE')
-                        <button title="Delete" class="btn btn-danger">Delete</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div class="row">
-        <div class="col-12">
-            {{$books->links()}}
-        </div>
-    </div>
-</div>
 @endsection
 
 @section('script')
-@vite('resources/js/delete.js')
+    @vite('resources/js/delete.js')
 @endsection
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,29 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($books as $book)
+                <div class="col-3 my-3">
 
-<div class="container">
-    <div class="row">
-        @foreach($books as $book)
-        <div class="col-3 my-3">
-
-            <div class="card" style="width: 18rem;">
-                <img src="{{$book->cover_image}}" class="card-img-top" alt="{{$book->title}}">
-                <div class="card-body">
-                    <h5 class="card-title">{{$book->title}}</h5>
-                    <p class="card-text">{{$book->description}}</p>
-                    <a href="{{route('guest.show', $book->id)}}" class="btn btn-primary">Show</a>
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $book->title }}</h5>
+                            <p class="card-text">{{ $book->description }}</p>
+                            <a href="{{ route('guest.show', $book->id) }}" class="btn btn-primary"><i
+                                    class="fa-solid fa-eye"></i></a>
+                        </div>
+                    </div>
                 </div>
+            @endforeach
+        </div>
+        <div class="row">
+            <div class="col-12">
+                {{ $books->links() }}
             </div>
         </div>
-        @endforeach
     </div>
-    <div class="row">
-        <div class="col-12">
-            {{$books->links()}}
-        </div>
-    </div>
-</div>
-
-
 @endsection
