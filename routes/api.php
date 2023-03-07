a2952e2ad2026880229fa467c8e2827027b392b5
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-07 15:42:48 +0100

    Fix route

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/books', [ApiBookController::class, 'index'])->name('api.book.index');
+Route::get('/books/{book}', [ApiBookController::class, 'show'])->name('api.book.show');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/books', [ApiBookController::class, 'index'])->name('api.book.index');
-Route::get('/books/{book}', [ApiBookController::class, 'show'])->name('api.book.show');
