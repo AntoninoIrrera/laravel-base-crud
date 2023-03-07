dafa10da1db2c340750a213a001b05e9ee9d5b9d
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-03-07 15:39:25 +0100

    Create authentication-api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BookController as ApiBookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/books', [ApiBookController::class, 'index'])->name('api.book.index');
+Route::get('/books/{book}', [ApiBookController::class, 'show'])->name('api.book.show');
