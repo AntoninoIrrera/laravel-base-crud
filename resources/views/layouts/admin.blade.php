76ae120a582346f4233c1f9247dc12eeed83c19f
GiadaGallitto <gallitto.giada@gmail.com>
2023-03-07 17:33:29 +0100

    Add nav item for genres

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -56,6 +56,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/admin/roles') }}">{{ __('Roles') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/admin/genres') }}">{{ __('Genres') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
